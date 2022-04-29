61b210152aadf778ccef1672cb402d0bb610cea5
abbos <obboshon22@gmail.com>
2022-04-30 00:45:38 +0500

    first project

---
+++ js/index.js
@@ -0,0 +1,21 @@
+alert ("Saytga xush kelibsiz")
+var userName = prompt ("ismingiz ")
+console.log (userName);
+var age = prompt("How old are you ")
+console.log (age);
+var job = prompt ("what is your job ")
+console.log(job);
+var education = prompt("Where do you study")
+console.log(education);
+var course = prompt("In which course do you study")
+console.log(course);
+var sphere = prompt("Field of your study")
+console.log(sphere);
+var aim = prompt("Why do you want to apply for this company")
+console.log(aim);
+var wish = prompt("What do you wish after five years")
+console.log("wish");
+var responsibility = prompt("Can you take responsibility if you work here?")
+console.log("responsibility");
+var dream = prompt("What is your dream in the future?")
+console.log(dream);
